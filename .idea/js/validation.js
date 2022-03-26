6df075225c82d61e7013a35bb7f77bfb7ddef487
Jordan Bhar <bharjor@sheridancollege.ca>
2022-03-26 00:10:26 -0400

    Commit 3: Implemented Adding and Deleting Entries into the IndexedDB

--- .idea/js/validation.js
+++ .idea/js/validation.js
@@ -6,7 +6,7 @@ openRequest.onupgradeneeded = function(e) {
     var DB = e.target.result;
     if(!DB.objectStoreNames.contains("Company")) {
         //tables name is People
-        const companyTable = DB.createObjectStore("Company", {keyPath: "ID"});
+        const companyTable = DB.createObjectStore("Company", {keyPath: "id"});
     }
 };
 
@@ -19,18 +19,26 @@ openRequest.onerror = function(e) {
     // ..do error notification
 }
 
-
 function addCompany(ID, Name , CompanyName){
-    var persons = [{id: ID , name: Name, companyname: CompanyName ,created: new Date()}]
+    var person = {id: ID , name: Name, companyname: CompanyName ,created: new Date()}
 
     //open a read/write transaction
-    var transaction = db.transaction(["Company"],"readwrite");
+    var tx = makeTX("Company","readwrite");
 
     //create object store
-    var peopleStore = transaction.objectStore("Company");
+    var transaction = tx.objectStore("Company");
 
     //make a request to add a new item
-    var request = peopleStore.add(persons[0] , "1")
+    var request = transaction.add(person)
+
+    request.onsuccess = (ev) => {
+        console.log('successfully added an object');
+        //move on to the next request in the transaction or
+        //commit the transaction
+    };
+    request.onerror = (err) => {
+        console.log('error in request to add');
+    };
 
 }
 
@@ -38,76 +46,89 @@ function checkCompany(){
 
 }
 
+function deleteCompany(evt){
+
+    let id = document.getElementById("invid").value
 
 
-function deleteCompnay(){
+    var tx = makeTX("Company","readwrite");
+
+    //create object store
+    var transaction = tx.objectStore("Company");
+
+    //make a request to add a new item
+    var request = transaction.delete(id)
+
+    request.onsuccess = (ev) => {
+        console.log('successfully deleted an object');
+
+        //move on to the next request in the transaction or
+        //commit the transaction
+    };
+    request.onerror = (err) => {
+        console.log('error in request to delete');
+    };
 
 }
-
-
 
 FormEventListener()
 
 function FormEventListener() {
-
     let addButton = document.getElementById("addCompany").addEventListener("click", validateForm)
-
-
+    let deleteButton = document.getElementById("removeCompany").addEventListener("click", deleteCompany)
+    let checkButton = document.getElementById("checkCompany").addEventListener("click", null)
 }
 
 function validateForm(evt) {
 
     let isValid = false;
 
-    console.log("test")
-
     let id = document.getElementById("invid").value
     let name = document.getElementById("name").value
     let companyName = document.getElementById("companyname").value
 
-
-
     if (isEmpty(id)){
         document.getElementById("error1").hidden = true
-
     }else {
         document.getElementById("error1").hidden = false
-
     }
-
 
     if (isEmpty(name)){
         document.getElementById("error2").hidden = true
-
     }else {
         document.getElementById("error2").hidden = false
-
     }
 
     if (isEmpty(companyName)){
         document.getElementById("error3").hidden = true
-
     }else {
         document.getElementById("error3").hidden = false
-
     }
-
-
 
 
     isValid = id && name && companyName
 
     if (!isValid){
         console.log("Wrong")
+        //need to finish code here
     }else {
         addCompany(id, name, companyName)
+        //need to finish code here
     }
 
 }
 
-
 function isEmpty(input){
     return (input.length > 0)
 }
+
+function makeTX(storeName, mode) {
+    let tx = db.transaction(storeName, mode);
+    tx.onerror = (err) => {
+        console.warn(err);
+    };
+    return tx;
+}
+
 
 
